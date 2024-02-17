75049ed4304fa14347372fca133629ea1cb433dc
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-02-17 18:08:06 +0530

    add clerk authentication properly

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/api/webhook"],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = "/select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    } // Redirect to org selection if authenticated and on a public route (e.g. /)
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    } // Redirect to sign in if not authenticated and not a public route (e.g. /api/*)
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    } // Redirect to org selection if authenticated but no org selected
+  },
+});
+
+export const config = {
+  // Protects all routes, including api/trpc.
+  // See https://clerk.com/docs/references/nextjs/auth-middleware
+  // for more information about configuring your Middleware
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
