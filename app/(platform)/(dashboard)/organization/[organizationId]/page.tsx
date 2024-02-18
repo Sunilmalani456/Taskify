173d09f1d0be57c0f20808ce52c012d84fa0a4f7
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-02-18 23:14:49 +0530

    add <Info /> components  with popover and tooltip components along with BoardList components. and create a board from popover components.

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Separator } from "@/components/ui/separator";
+import BoardList from "./_components/board-list";
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = () => {
-  return <div className="">OrganizationIdPage</div>;
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        {/* <Suspense fallback={<BoardList.Skeleton />}> */}
+        <BoardList />
+        {/* </Suspense> */}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,10 +1,12 @@
+import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <div>{children}</div>
+      <Toaster />
+      {children}
     </ClerkProvider>
   );
 };
