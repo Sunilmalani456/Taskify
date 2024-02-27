d1f52b910c2feffb4af7f5aad77a83f580c28b04
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-02-27 17:31:27 +0530

    add drage and drope functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@prisma/client";
-// import { Draggable } from "@hello-pangea/dnd";
+import { Draggable } from "@hello-pangea/dnd";
 
 // import { useCardModal } from "@/hooks/use-card-modal";
 
@@ -14,12 +14,19 @@ export const CardItem = ({ data, index }: CardItemProps) => {
   //   const cardModal = useCardModal();
 
   return (
-    <div
-      role="button"
-      //   onClick={() => cardModal.onOpen(data.id)}
-      className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
-    >
-      {data.title}
-    </div>
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          //   onClick={() => cardModal.onOpen(data.id)}
+          className="truncate border-1 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
   );
 };
